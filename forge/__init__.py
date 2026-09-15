5b8d8032f7f86e9f5521e1d4d048d98d84a337b5
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-15 19:36:58 +0530

    feat: expose project builder as public Forge API

---
+++ forge/__init__.py
@@ -0,0 +1,5 @@
+"""Forge AI public package surface."""
+
+from forge.project_builder import ProjectBuildResult, ProjectBuilder, build_project
+
+__all__ = ["ProjectBuildResult", "ProjectBuilder", "build_project"]
