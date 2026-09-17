91d87709fe8a3e585c19b3323cf36f32a8dc6558
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-17 17:25:49 +0530

    feat: export worker admission primitives

--- forge/workers/__init__.py
+++ forge/workers/__init__.py
@@ -1,5 +1,9 @@
 """Remote worker control-plane primitives."""
 
+from forge.workers.admission import Admission, AdmissionError, WorkerAdmission, WorkerRequirements
 from forge.workers.registry import WorkerRecord, WorkerRegistry
 
-__all__ = ["WorkerRecord", "WorkerRegistry"]
+__all__ = [
+    "Admission", "AdmissionError", "WorkerAdmission", "WorkerRequirements",
+    "WorkerRecord", "WorkerRegistry",
+]
