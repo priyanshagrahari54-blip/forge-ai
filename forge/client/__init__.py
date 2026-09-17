da1de0ed7e5e95a443ce8d8e62581d51a4af2097
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-17 17:39:10 +0530

    Export Forge thin-client profiles

---
+++ forge/client/__init__.py
@@ -0,0 +1,4 @@
+"""Client-side profiles for resource-constrained Forge endpoints."""
+from forge.client.thin_client import G560_PROFILE, ThinClientProfile, select_profile
+
+__all__ = ["G560_PROFILE", "ThinClientProfile", "select_profile"]
